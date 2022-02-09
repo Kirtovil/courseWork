c12eae54c3075fd0e72c45df4dbe8175ee98a6fc
Alexandr Polikarpov <nub.and.co@gmail.com>
2022-02-09 21:53:48 +0300

    второй

--- src/pro/sky/Main.java
+++ src/pro/sky/Main.java
@@ -72,21 +72,18 @@ public class Main {
 
     public static void main(String[] args) {
         Employee[] employeeArray = new Employee[10];
-        for (int i = 0; i < employeeArray.length; i++) {
-            if (employeeArray[i] == null) {
 
-                addEmployee(employeeArray, new Employee("Shishkin", "Vasily", "Ivanovich", 1, 10_660));
-                addEmployee(employeeArray, new Employee("Zdravkin", "Aleksey", "Sergeevich", 3, 50_000));
-                addEmployee(employeeArray, new Employee("Kashin", "Aleksandr", "Aleksandrovich", 4, 60_000));
-                addEmployee(employeeArray, new Employee("Strelcov", "Sergey", "Vladimirovich", 1, 10_500));
-                addEmployee(employeeArray, new Employee("Iliyn", "Vasily", "Stepanovich", 5, 12_648));
-                addEmployee(employeeArray, new Employee("Shevchuck", "Viktor", "Andreevich", 1, 17_500));
-                addEmployee(employeeArray, new Employee("Sabakin", "Michail", "Kirilovich", 3, 19_600));
-                addEmployee(employeeArray, new Employee("Timofeeva", "Katia", "Vasilieva", 1, 80_080));
-                addEmployee(employeeArray, new Employee("Shonova", "lydmila", "Michaylovna", 4, 65_200));
-                addEmployee(employeeArray, new Employee("Vontova", "Larisa", "Ivanovna", 5, 8_015));
-            }
-        }
+        addEmployee(employeeArray, new Employee("Kashin", "Aleksandr", "Aleksandrovich", 4, 60_000));
+        addEmployee(employeeArray, new Employee("Shishkin", "Vasily", "Ivanovich", 1, 10_660));
+        addEmployee(employeeArray, new Employee("Zdravkin", "Aleksey", "Sergeevich", 3, 50_000));
+        addEmployee(employeeArray, new Employee("Strelcov", "Sergey", "Vladimirovich", 1, 10_500));
+        addEmployee(employeeArray, new Employee("Iliyn", "Vasily", "Stepanovich", 5, 12_648));
+        addEmployee(employeeArray, new Employee("Shevchuck", "Viktor", "Andreevich", 1, 17_500));
+        addEmployee(employeeArray, new Employee("Sabakin", "Michail", "Kirilovich", 3, 19_600));
+        addEmployee(employeeArray, new Employee("Timofeeva", "Katia", "Vasilieva", 1, 80_080));
+        addEmployee(employeeArray, new Employee("Shonova", "lydmila", "Michaylovna", 4, 65_200));
+        addEmployee(employeeArray, new Employee("Vontova", "Larisa", "Ivanovna", 5, 8_015));
+
         printEmployees(employeeArray);
         System.out.println("Месячные выплаты зарплаты сотрудникам - " + calculateSalary(employeeArray) + " руб.");
         System.out.println("минимальная месячная зарпалта - " + minSalary(employeeArray) + " руб.");
